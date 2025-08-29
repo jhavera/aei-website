aea750bc7c22440377f7ac6d1cbaaf5040d77813
jhavera <jhavera@gmail.com>
2025-08-29 07:07:01 -0400

    Added code to get PORT environment variable for set default to 5000

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +9,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable 'PORT' or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
